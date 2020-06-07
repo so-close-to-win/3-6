3411d1aa43ed5289a135a867b38079cc2aa676e6
so-close-to-win <rinko1992@gmail.com>
2020-06-07 04:33:42 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 
 
-def test_button(browser):
+def test_button_exist(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     assert browser.find_element_by_css_selector("button.btn.btn-lg.btn-primary.btn-add-to-basket")
